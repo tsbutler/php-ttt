8667083632461d390231013696fe9446293f8c21
Thorin Butler <thorin.butler@gmail.com>
2016-06-29 20:00:31 -0500

    seem to have made a rudimentary grid in flexbox. Now I just need to change the content when a player clicks there

--- index.php
+++ index.php
@@ -25,16 +25,22 @@ session_start();
     $turn_counter = 0;
   ?>
 
-  <div class="container">
-    <div class="box box0"><?php echo $_SESSION["box0"]; ?></div>
-    <div class="box box1"><?php echo $_SESSION["box1"]; ?></div>
-    <div class="box box2"><?php echo $_SESSION["box2"]; ?></div>
-    <div class="box box3"><?php echo $_SESSION["box3"]; ?></div>
-    <div class="box box4"><?php echo $_SESSION["box4"]; ?></div>
-    <div class="box box5"><?php echo $_SESSION["box5"]; ?></div>
-    <div class="box box6"><?php echo $_SESSION["box6"]; ?></div>
-    <div class="box box7"><?php echo $_SESSION["box7"]; ?></div>
-    <div class="box box8"><?php echo $_SESSION["box8"]; ?></div>
+  <div class="grid">
+    <div class="grid-row row1">
+      <div class="box box0"><?php echo $_SESSION["box0"]; ?></div>
+      <div class="box box1"><?php echo $_SESSION["box1"]; ?></div>
+      <div class="box box2"><?php echo $_SESSION["box2"]; ?></div>
+    </div>
+    <div class="grid-row row2">  
+      <div class="box box3"><?php echo $_SESSION["box3"]; ?></div>
+      <div class="box box4"><?php echo $_SESSION["box4"]; ?></div>
+      <div class="box box5"><?php echo $_SESSION["box5"]; ?></div>
+    </div>
+    <div class="grid-row row3">
+      <div class="box box6"><?php echo $_SESSION["box6"]; ?></div>
+      <div class="box box7"><?php echo $_SESSION["box7"]; ?></div>
+      <div class="box box8"><?php echo $_SESSION["box8"]; ?></div>
+    </div>  
   </div>  
 
 </body>
