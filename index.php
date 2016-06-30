0f7b0b02a65d3f96fac72dcfae0efd0de916455b
Thorin Butler <thorin.butler@gmail.com>
2016-06-29 19:23:24 -0500

    adding session variables that I'm going to try to alter as this goes forwards

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+//Start the session
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Tic | Tac | Toe</title>
+</head>
+<body>
+
+  <?php
+    $_SESSION["box0"] = "-";
+    $_SESSION["box1"] = "-";
+    $_SESSION["box2"] = "-";
+    $_SESSION["box3"] = "-";
+    $_SESSION["box4"] = "-";
+    $_SESSION["box5"] = "-";
+    $_SESSION["box6"] = "-";
+    $_SESSION["box7"] = "-";
+    $_SESSION["box8"] = "-";
+
+    
+    echo $_SESSION["box0"] = "-";
+    echo $_SESSION["box1"] = "-";
+    echo $_SESSION["box2"] = "-";
+    echo $_SESSION["box3"] = "-";
+    echo $_SESSION["box4"] = "-";
+    echo $_SESSION["box5"] = "-";
+    echo $_SESSION["box6"] = "-";
+    echo $_SESSION["box7"] = "-";
+    echo $_SESSION["box8"] = "-";
+  ?>
+
+</body>
+</html>
